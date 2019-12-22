17d52d69bf3223f60965b84fda4bcbb7046bf8ed
leonardo_zapparoli <leo.zapparoli@gmail.com>
2019-12-22 15:03:21 -0300

    first commit

---
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,17 @@
+class DateHelper {
+  constructor() {
+    throw new Error("DateHelper não pode ser instanciada");
+  }
+
+  static textoParaData(texto) {
+    if (!/\d{4}-\d{2}-\d{2}/.test(texto)) {
+      throw new Error("Deve ser no formato yyyy-mm-dd");
+    }
+    let valores = texto.split("-").map((item, indice) => item - (indice % 2));
+    return new Date(...valores);
+  }
+
+  static dataParaTexto(data) {
+    return `${data.getMonth() + 1}/${data.getDate()}/${data.getFullYear()}`;
+  }
+}
